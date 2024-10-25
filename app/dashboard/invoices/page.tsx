917acff3ad227024101c7ffc7ae00d25852bbec8
lumen nganje <lumennganje592@gmail.com>
2024-10-25 05:14:29 +0100

    acme-logo.tsx updated

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div className="bg-blue-500 h-44">page</div>
+  )
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div className="h-64 bg-purple-500">page</div>
+  )
+}
+
+export default page
